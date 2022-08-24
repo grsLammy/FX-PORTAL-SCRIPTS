3ae1055739b20a26d5ee606c8a24ad18cd77cccc
grsLammy <grspro98@gmail.com>
2022-08-25 01:37:40 +0530

    feat: add choice 6 for withdrawExit

--- script/startScript.js
+++ script/startScript.js
@@ -5,6 +5,7 @@ const ERC20TokenOperations = require("./ERC20/ERC20TokenOperations");
 const ERC721TokenOperations = require("./ERC721/ERC721TokenOperations");
 const ERC1155TokenOperations = require("./ERC1155/ERC1155TokenOperations");
 const MintableERC20TokenOperations = require("./MintableERC20/MintableERC20TokenOperations");
+const { withdrawExit } = require("./utils/withdrawExit");
 
 async function startScript() {
     console.log("\n-----------------------------------------");
@@ -16,11 +17,19 @@ async function startScript() {
     console.log("2. ERC20 Token Operations.");
     console.log("3. ERC721 Token Operations.");
     console.log("4. ERC1155 Token Operations.");
-    console.log("5. Mintable ERC20 Token Operations.\n");
+    console.log("5. Mintable ERC20 Token Operations.");
+    console.log("6. Submit burn txn proof using withdrawExit Operation.\n");
     const choice = prompt("Enter your choice: ");
 
     if (!choice) return console.log("Choice cannot be null");
-    if (choice !== "1" && choice !== "2" && choice !== "3" && choice !== "4" && choice !== "5")
+    if (
+        choice !== "1" &&
+        choice !== "2" &&
+        choice !== "3" &&
+        choice !== "4" &&
+        choice !== "5" &&
+        choice !== "6"
+    )
         return console.log(`Transaction ${choice} is unsupported`);
 
     try {
@@ -29,6 +38,7 @@ async function startScript() {
         if (choice === "3") await ERC721TokenOperations();
         if (choice === "4") await ERC1155TokenOperations();
         if (choice === "5") await MintableERC20TokenOperations();
+        if (choice === "6") await withdrawExit();
         return;
     } catch (error) {
         console.log(`Error at startScript: ${error}`);
@@ -37,7 +47,7 @@ async function startScript() {
 
 startScript()
     .then(() => {
-        console.log("\n\n---------- ENDING ALL PROCESS ----------.\n\n");
+        console.log("\n\n---------- ENDING ALL PROCESS ----------\n\n");
         process.exit(0);
     })
     .catch((error) => {
