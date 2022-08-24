83d51edbc6856f3cfabaa4b24bb50d3951a386c5
grsLammy <grspro98@gmail.com>
2022-08-24 21:23:32 +0530

    feat: redeploy new sample tokens

--- src/rootERC20Token.sol
+++ src/rootERC20Token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract rootERC20Token is ERC20, Ownable {
-    constructor() ERC20("ERC20 TEST TOKEN", "rootERC20Token") {
+    constructor() ERC20("Root ERC20 Token", "rootERC20Token") {
         _mint(_msgSender(), 1000000000 * 1e18);
     }
 }
