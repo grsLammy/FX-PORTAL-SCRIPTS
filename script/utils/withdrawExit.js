16a8688553d08d649a4b8e771a96af7474cc753b
grsLammy <grspro98@gmail.com>
2022-08-25 01:38:14 +0530

    update: withdrawExit.js

--- script/utils/withdrawExit.js
+++ script/utils/withdrawExit.js
@@ -1,18 +1,32 @@
-const { getFxPortalClient } = require("./FxPortalClient");
+const ps = require("prompt-sync");
+const prompt = ps();
 const config = require("../../config");
+const { getFxPortalClient } = require("./FxPortalClient");
 
 const token = config.RootERC20Token;
-async function withdrawExit(burnTxHash) {
-    // initiate fxClient
-    console.log("token: ", token);
-    const fxClient = await getFxPortalClient();
-    console.log("FxClient: ", fxClient);
-    const erc20Token = fxClient.erc20(token, true);
-    console.log("erc20Token: ", erc20Token);
-    // execute transaction
-    const txResult = await erc20Token.withdrawExit(burnTxHash);
-    const txHash = await txResult.getTransactionHash();
-    console.log("Transaction Hash:", txHash);
+async function withdrawExit() {
+    try {
+        console.log("\n-----------------------------------------");
+        console.log("INITIATING WITHDRAWEXIT PROCESS");
+        console.log("-----------------------------------------\n");
+
+        const burnTxnHash = prompt("Enter the burn transaction hash: ");
+        if (!burnTxnHash) return console.log("Message cannot be null");
+        //if (burnTxnHash.length !== 42) return console.log(`${burnTxnHash} is not a valid address`);
+        // initiate fxClient
+        console.log("token: ", token);
+        const fxClient = await getFxPortalClient();
+        console.log("FxClient: ", fxClient);
+        const erc20Token = fxClient.erc20(token, true);
+        console.log("erc20Token: ", erc20Token);
+        // execute transaction
+        const txResult = await erc20Token.withdrawExit(burnTxnHash);
+        const txHash = await txResult.getTransactionHash();
+        console.log("Transaction Hash:", txHash);
+    } catch (error) {
+        console.log("Error at withdrawExit", error);
+        process.exit(1);
+    }
 }
 
 module.exports = { withdrawExit };
